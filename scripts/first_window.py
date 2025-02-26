4775517f29a16402210cfd162961bab8adef24e7
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-26 14:20:49 +0100

    Add files via upload

---
+++ scripts/first_window.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import turtle
+
+screen_x = 512
+screen_y = 512
+
+sc = turtle.Screen()
+sc.setup(screen_x, screen_y)
+
+window_title = "First Turtle Graphics Demo"
+background_color = "red"
+
+turtle.title(window_title)
+turtle.bgcolor(background_color)
+
+turtle.done()
