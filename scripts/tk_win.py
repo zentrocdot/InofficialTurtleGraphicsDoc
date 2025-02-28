27d579342120b00945425ef2759fa00a43b4325c
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-28 16:35:23 +0100

    Add files via upload

---
+++ scripts/tk_win.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Import tkinter as tk.
+import tkinter as tk
+
+# Create two top-level windows.
+win_0 = tk.Tk()
+win_1 = tk.Tk()
+
+# Keep the two windows open.
+tk.mainloop()
